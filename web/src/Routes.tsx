d66b2138b96b850553b5d72f7bc7dca7ed2ecdce
Cameron <69396582+camer0nluo@users.noreply.github.com>
2023-12-11 00:26:32 -0800

    feature: add rsvp routes to auth routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -13,6 +13,9 @@ const Routes = () => {
           <Route path="/dashboard" page={DashboardPage} name="dashboard" />
           <Route path="/group/invite/{id:String}" page={EventInvitePage} name="invite" />
           <Route path="/event" page={EventPage} name="event" />
+          <Route path="/rsvp/{id:String}" page={RSVPPage} name="rsvp" />
+          <Route path="/rsvp/{id:String}/rsvp-decline" page={RsvpDeclinePage} name="rsvpDecline" />
+          <Route path="/rsvp/{id:String}/rsvp-accept" page={RsvpAcceptPage} name="rsvpAccept" />
           <Route path="/event-dashboard" page={EventDashboardPage} name="eventDashboard" />
         </Set>
       </PrivateSet>
@@ -22,9 +25,6 @@ const Routes = () => {
         <Route path="/logout" page={LogoutPage} name="logout" />
         <Route path="/login" page={LoginPage} name="login" />
         <Route path="/" page={HomePage} name="home" />
-        <Route path="/rsvp/{id:String}" page={RSVPPage} name="rsvp" />
-        <Route path="/rsvp-decline" page={RsvpDeclinePage} name="rsvpDecline" />
-        <Route path="/rsvp-accept" page={RsvpAcceptPage} name="rsvpAccept" />
         <Route notfound page={NotFoundPage} />
       </Set>
     </Router>
